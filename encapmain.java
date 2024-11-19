f5dddc8ab74872396078f01a5c2f41e5dc55a1c2
parker <54675124+Lourdez@users.noreply.github.com>
2024-11-19 11:35:18 +0530

    Update encapmain.java

--- encapmain.java
+++ encapmain.java
@@ -1,1 +1,29 @@
 
+package encapsulation;
+
+import java.util.Scanner;
+
+public class DemoConst {
+
+	public static void main(String args[]) {
+		System.out.println("hi , please Enter the String  or wrong");
+
+		Scanner n = new Scanner(System.in);
+
+		System.out.println("You entered the phase :" + n);
+		DemoConst o1 = new DemoConst();
+		o1.demoname();
+		o1.username();
+		n.close();
+	}
+
+	private void demoname() {
+		System.out.println("this is the method of overloading");
+
+	}
+
+	public void username() {
+
+		System.out.println("Method overriding");
+	}
+}
